e986d71f9b4c9bde64768ad6bb44268714f38a8b
Patrick Gaines <patrickg.codes@gmail.com>
2020-08-06 20:33:26 -0500

    Fixed Auth UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import { withAuthenticator } from 'aws-amplify-react';
+import React, { Fragment } from 'react';
+import { withAuthenticator, AmplifyGreetings, AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import DisplayPosts from './components/DisplayPosts';
 import CreatePost from './components/CreatePost';
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <CreatePost />
-        <DisplayPosts />
-    </div>
-  );
+    const [authState, setAuthState] = React.useState();
+    const [user, setUser] = React.useState();
+
+    React.useEffect(() => {
+        return onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData)
+        });
+    }, []);
+
+    return authState === AuthState.SignedIn && user ? (
+        <Fragment>
+            <AmplifyGreetings username={user.username}></AmplifyGreetings>
+            <div className="App">
+                <CreatePost />
+                <DisplayPosts />
+            </div>
+        </Fragment>
+    ) : (
+      <AmplifyAuthenticator />
+    );
 }
 
-export default withAuthenticator(App, { includeGreetings: true });
+export default withAuthenticator(App);
